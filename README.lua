0d712d7254a7d05bef6a2aaa899582725944d98a
lucascout1 <145732625+lucascout1@users.noreply.github.com>
2023-09-21 22:56:33 -0300

    README.lua
    README :)

---
+++ README.lua
@@ -0,0 +1,11 @@
+local MyAbout = {
+  
+  Im a 13 year old scripter looking towards making stuff just for fun,
+  I love 3d engines, physics, and BAANANI,
+  If you have any questions feel free to ask
+  
+}
+
+  return MyAbout
+
+  -- Hehe
